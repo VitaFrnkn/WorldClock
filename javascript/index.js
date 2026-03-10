7a7a06a784f553bdafba9496f5b8f0df02e5571c
CyberVi00 <159148762+VitaFrnkn@users.noreply.github.com>
2026-03-09 22:31:24 -0600

    debugging

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,24 @@
 function updateTime(){
 //Los Angeles time update
     let losAngelesElement = document.querySelector("#los-angeles");
-    let losAngelesDateElement = losAngelesElement.querySelector(".date");
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-    let losAngelesTime = moment().tz("America/Los_Angeles");
+    if (losAngelesElement.length){
+        let losAngelesDateElement = losAngelesElement.querySelector(".date");
+        let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+        let losAngelesTime = moment().tz("America/Los_Angeles");
 
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
-
+        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+        losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 // Rome time update
     let RomeElement = document.querySelector("#rome");
-    let RomeDateElement = RomeElement.querySelector(".date");
-    let RomeTimeElement = RomeElement.querySelector(".time");
-    let RomeTime = moment().tz("Europe/Rome");
+    if (RomeElement.length){
+        let RomeDateElement = RomeElement.querySelector(".date");
+        let RomeTimeElement = RomeElement.querySelector(".time");
+        let RomeTime = moment().tz("Europe/Rome");
 
-    RomeDateElement.innerHTML = RomeTime.format("MMMM Do YYYY");
-    RomeTimeElement.innerHTML = RomeTime.format("h:mm:ss [<small>]A[</small>]");
-
+        RomeDateElement.innerHTML = RomeTime.format("MMMM Do YYYY");
+        RomeTimeElement.innerHTML = RomeTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 }
 
 function updateCity(event) {
