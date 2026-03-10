284d3560cfac40957ed20b1de61c7862102552af
CyberVi00 <159148762+VitaFrnkn@users.noreply.github.com>
2026-03-09 20:39:52 -0600

    Rome time adjustment

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,20 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
+function updateTime(){
+//Los Angeles time update
+    let losAngelesElement = document.querySelector("#los-angeles");
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+
+// Rome time update
+    let RomeElement = document.querySelector("#rome");
+    let RomeDateElement = RomeElement.querySelector(".date");
+    let RomeTimeElement = RomeElement.querySelector(".time");
+    let RomeTime = moment().tz("Europe/Rome");
+
+    RomeDateElement.innerHTML = RomeTime.format("MMMM Do YYYY");
+    RomeTimeElement.innerHTML = RomeTime.format("h:mm:ss [<small>]A[</small>]");
+
+}
