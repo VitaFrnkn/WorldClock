c2baf1141ddb034d6f5df0493fbcc56c8de833a9
CyberVi00 <159148762+VitaFrnkn@users.noreply.github.com>
2026-03-09 22:23:54 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -31,7 +31,7 @@ function updateCity(event) {
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-    <div class="time">${cityName.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
     </div>`;
 
 }
