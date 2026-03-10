8901ce5b29b82f3087b3dcedc414048bfb5cba66
CyberVi00 <159148762+VitaFrnkn@users.noreply.github.com>
2026-03-10 09:21:31 -0600

    checking interval time works with any selection

--- javascript/index.js
+++ javascript/index.js
@@ -42,8 +42,8 @@ function updateCity(event) {
 }
 
 updateTime();
-setInterval(updateTime, 1000);
 
 let citiesSelect = document.querySelector("#city");
-
 citiesSelect.addEventListener("change",updateCity);
+
+setInterval(updateTime, 1000);
